242f2885f8f26001f6ee5735d72ecb27d1bc517b
Clevrem <clevrem2@gmail.com>
2025-03-21 11:48:55 +0500

    trendingni ui qismi qoldi backend bilan ulash yakunlandi.

---
+++ lib/misol.dart
@@ -0,0 +1,14 @@
+void main() {
+  Clevrem<num>(5, 6);
+  Clevrem<String>("Bekzod", "Clevrem");
+}
+
+T? Clevrem<T>(T? value, T? value2) {
+  if (value is num && value2 is num) {
+    print("bu num shaklida ${value * value2}");
+    return (value * value2) as T;
+  } else if (value is String || value2 is String) {
+    print("bu String shaklida yig'indini topib bo'lmaydi!!!");
+    return null;
+  }
+}
